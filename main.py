50ba304f50010c3b23424829027c8e162c1aea92
kwwl <155566997+kwwl@users.noreply.github.com>
2024-01-29 08:53:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from data_preparation import DataPreparation
+from regression import Regression
+
+
+csv_path = "./vente_maillots_de_bain(1).csv"
+data_preparation_object = DataPreparation(csv_path)
+regression_object = Additif(data_preparation_object)
+
+# data_preparation_object.show_graph()
