ecaa86a03d7fa1b772d1c28a02a97acbed65d290
kwwl <155566997+kwwl@users.noreply.github.com>
2024-01-29 10:03:53 +0100

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from data_preparation import DataPreparation
-from regression import Regression
+from additif import Additif
 
 
-csv_path = "./vente_maillots_de_bain(1).csv"
+csv_path = (
+    "/Users/kemillamouri/Desktop/projet_Hakim/Additif-Model/vente_maillots_de_bain.csv"
+)
+
 data_preparation_object = DataPreparation(csv_path)
 regression_object = Additif(data_preparation_object)
 
