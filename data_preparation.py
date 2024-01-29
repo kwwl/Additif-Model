6c4a5602342ff8f82d7113891cb660fc7b6ce679
kwwl <155566997+kwwl@users.noreply.github.com>
2024-01-29 10:03:29 +0100

    data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,43 +1,46 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 class DataPreparation:
     def __init__(self, csv_path):
-        """
-        Cette classe prend en entrée un chemin de fichier csv.
-        Elle split le jeu de donnée en 2 bases
-        + une train 75 %
-        + une test 25 %
-        Ce 2 bases, la classe va les splits en 2
+        self.dataset_df = pd.read_csv(csv_path)
+        self.dataset_df["Years"] = pd.to_datetime(
+            self.dataset_df["Years"]
+        )  # 1. Transformer la colonne "Years" en format date
+        self.dataset_df["month_name"] = self.dataset_df[
+            "Years"
+        ].dt.month_name()  # 2. Extraire le nom du mois sur une nouvelle colonne nommée "month_name"
+        self.month_dummies = pd.get_dummies(
+            self.dataset_df["month_name"], prefix="month"
+        )  # 3. Transformer la nouvelle colonne "month_name" grâce au one-hot encoding
+        for col in self.month_dummies.columns:
+            self.dataset_df[col] = self.month_dummies[col]
 
-        + un vecteur x (qui contient les indexs temporels)
-        + un vecteyr y (qui contient les valeurs à prédire)
-        En tout cette va extraire 4 arrays.
-        x_train
-        y_train
-        x_test
-        y_test
-        """
-        self.dataset_df = pandas.read_csv(csv_path)
-        self.dataset_df["month"] = pandas.to_datetime(self.dataset_df["month"])
+        # Filtrer les données jusqu'en 2007
+        self.dataset_df = self.dataset_df[self.dataset_df["Years"].dt.year <= 2006]
+
         self.prepare_data()
 
     def prepare_data(self):
         number_of_rows = len(self.dataset_df)
-        self.dataset_df["index_mesure"] = numpy.arange(0, number_of_rows, 1)
+        self.dataset_df["index_mesure"] = np.arange(0, number_of_rows, 1)
 
         dataset_train_df = self.dataset_df.iloc[: int(number_of_rows * 0.75)]
         dataset_test_df = self.dataset_df.iloc[int(number_of_rows * 0.75) :]
 
-        self.x_train = dataset_train_df[["index_mesure"]].values
+        self.x_train = dataset_train_df[
+            ["index_mesure"] + list(self.month_dummies.columns)
+        ].values
         self.y_train = dataset_train_df[["Sales"]].values
 
-        self.x_test = dataset_test_df[["index_mesure"]].values
+        self.x_test = dataset_test_df[
+            ["index_mesure"] + list(self.month_dummies.columns)
+        ].values
         self.y_test = dataset_test_df[["Sales"]].values
 
     def show_graph(self):
         plt.figure(figsize=(15, 6))
-        plt.plot(self.dataset_df["month"], self.dataset_df["Sales"], "o:")
+        plt.plot(self.dataset_df["Years"], self.dataset_df["Sales"], "x:")
         plt.show()
